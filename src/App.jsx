72c185d88a03b63cf57c6f0709332992d171773c
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-11-19 15:47:44 +0100

    recreate post blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,96 @@
 import { useState } from 'react'
+
 import './App.css'
+
+const basePosts = [
+  {
+    id: 1,
+    title: "Titolo del Post",
+    image: './images/1.jpg',
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["html", "css"],
+    published: true,
+  },
+  {
+    id: 2,
+    title: "Titolo del Post",
+    image: './images/2.jpg',
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["js", "css"],
+    published: true,
+  },
+  {
+    id: 3,
+    title: "Titolo del Post",
+    image: './images/3.jpg',
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["js", "php"],
+    published: true,
+  },
+  {
+    id: 4,
+    title: "Titolo del Post",
+    image: './images/4.jpg',
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["html"],
+    published: false,
+  },
+];
 
 function App() {
 
+  const [posts, setPosts] = useState(basePosts)
+  const [newPost, setNewPost] = useState('')
+
+  function addPost(e) {
+    e.preventDefault()
+
+    setPosts([...posts, newPost])
+    setNewPost('')
+  }
+
+  function handleDelete(index) {
+    const newPosts = posts.filter((post, i) => i !== index)
+    setPosts(newPosts)
+  }
 
   return (
     <>
+      <div className="container">
+        <h1>React Blog Form</h1>
 
+        <form onSubmit={addPost}>
+          <div className="mb-3">
+            <label htmlFor="post" className="form-label">Post</label>
+            <div className="input-group mb-3">
+
+
+              <input type="text" className="form-control" placeholder="Aggiungi un post" aria-label="Aggiungi un post" aria-describedby="button-addon2" value={newPost} onChange={e => setNewPost(e.target.value)} />
+              <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Invia</button>
+            </div>
+
+
+          </div>
+        </form>
+
+
+        <ul className="list-group">
+          {posts.map((post, index) => (
+            <li key={index} className="list-group-item d-flex justify-content-between">
+              {post}
+              <button onClick={() => handleDelete(index)}><i className="bi bi-trash"></i></button>
+            </li>
+          ))}
+
+
+
+        </ul>
+
+      </div >
     </>
   )
 }
